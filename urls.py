7d726fd11d4a57d7b15990c9ae421981b57126a6
charalampos papaloizou <papaloizouc@gmail.com>
2014-07-27 11:14:47 +0100

    reuse template

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns(
     '',
     (r'^appengine_sessions/', include('appengine_sessions.urls')),
     (r'', include('core.urls')),
-    (r'', include('blog.urls', namespace='blog')),
+    (r'', include('blog.urls', namespace='blog', app_name='blog')),
 )
